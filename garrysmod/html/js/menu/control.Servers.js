8659083a59fcf8442206420f06849735e1614581
DarthTealc <DarthTealc@users.noreply.github.com>
2026-07-08 05:33:37 +1000

    Reset stale gamemode details on server list refresh (#2489)
    Improves https://github.com/Facepunch/garrysmod-issues/issues/6916

--- garrysmod/html/js/menu/control.Servers.js
+++ garrysmod/html/js/menu/control.Servers.js
@@ -77,6 +77,7 @@ function ControllerServers( $scope, $element, $rootScope, $location )
 		//
 		ServerTypes[ RootScope.ServerType ].gamemodes = {};
 		ServerTypes[ RootScope.ServerType ].list.length = 0;
+		ResetGamemodeInfo();
 
 		if ( !IN_ENGINE ) TestUpdateServers( RootScope.ServerType, RequestNum[ RootScope.ServerType ] );
 
